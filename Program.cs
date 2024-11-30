362254b42d03d5820db590b6ab76fb8889f57bcf
Matthieu <matthieu@sanerweb.com>
2024-11-30 15:28:47 +0100

    - Use custom function

--- Program.cs
+++ Program.cs
@@ -13,43 +13,29 @@ if (string.IsNullOrEmpty(apiKey))
     Console.WriteLine("API key is not set. Please set the OPEN_AI_SERVICE_KEY environment variable.");
     return;
 }
-var modelId = "gpt-35-turbo";
+var modelId = "gpt-35-turbo"; // Other models produce side effects so I stick to the one of the training
 
-// Build Semantic Kernel
+// Create a new kernel builder
 var builder = Kernel.CreateBuilder();
+
 // Add the OpenAI Chat completion plugin to the kernel builder
 builder.AddAzureOpenAIChatCompletion(deploymentName, endPoint, apiKey, modelId);
-// Add the core plugins to the kernel builder for timer and logging
-//builder.Plugins.AddFromType<TimePlugin>();
-// Add the ConversationSummaryPlugin to the kernel builder
-builder.Plugins.AddFromType<ConversationSummaryPlugin>();
 
-var kernel = builder.Build();    
+// Build the kernel
+var customKernel = builder.Build();   
 
-var prompts = kernel.ImportPluginFromPromptDirectory("Prompts/TravelPlugins");
+// Import the custom MusicLibraryPlugin
+customKernel.ImportPluginFromType<MusicLibraryPlugin>();
 
-//Import the plugins you created. You also use a ChatHistory object to store the user's conversation. Finally, pass some information to the SuggestDestinations prompt and record the results. 
-//Next, let's ask the user where they want to go so we can recommend some activities to them.
-ChatHistory history = [];
-string input = @"I'm planning an anniversary trip with my spouse. We like hiking, 
-    mountains, and beaches. Our travel budget is $15000";
-
-var result = await kernel.InvokeAsync<string>(prompts["SuggestDestinations"],
-    new() {{ "input", input }});
-
-Console.WriteLine(result);
-history.AddUserMessage(input);
-history.AddAssistantMessage(result);
-
-//In this code, you get some input from the user to find out where they want to go. 
-//Then call the SuggestActivities prompt with the destination and the conversation history.
-Console.WriteLine("Where would you like to go?");
-input = Console.ReadLine();
-
-result = await kernel.InvokeAsync<string>(prompts["SuggestActivities"],
+// Invoke the custom plugin AddToRecentlyPlayed function
+var result = await customKernel.InvokeAsync(
+    "MusicLibraryPlugin", 
+    "AddToRecentlyPlayed", 
     new() {
-        { "history", history },
-        { "destination", input },
+        ["artist"] = "Tiara", 
+        ["song"] = "Danse", 
+        ["genre"] = "French pop, electropop, pop"
     }
 );
+    
 Console.WriteLine(result);
