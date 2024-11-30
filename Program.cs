f931da152cb4c5ef190c01c62821b663bc974acf
Matthieu <matthieu@sanerweb.com>
2024-11-30 09:28:58 +0100

    - Add time plugin experiment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.SemanticKernel; // For OpenAI Service interaction
+﻿#pragma warning disable SKEXP0050 // Using the 'Microsoft.SemanticKernel' namespace is not recommended. Use 'Microsoft.SemanticKernel.Plugins.Core' instead.
+
+using Microsoft.SemanticKernel; // For OpenAI Service interaction
+using Microsoft.SemanticKernel.Plugins.Core; // For OpenAI Service interaction
 
 
 //Define OpenAI Service connection details
@@ -6,14 +9,16 @@ var deploymentName = "MastercamCopilot";
 var endPoint = "https://az2005-training.openai.azure.com/";
 var apiKey = Environment.GetEnvironmentVariable("OPEN_AI_SERVICE_KEY");
 var modelId = "gpt-4o";
-const string switchInputMode = "Switch input mode";
 
 // Build Semantic Kernel
 var builder = Kernel.CreateBuilder();
+// Add the OpenAI Chat completion plugin to the kernel builder
 builder.AddAzureOpenAIChatCompletion(deploymentName, endPoint, apiKey, modelId);
+// Add the core plugins to the kernel builder for timer and logging
+builder.Plugins.AddFromType<TimePlugin>();
+
 var kernel = builder.Build();       
 
-var result = await kernel.InvokePromptAsync("Give me a list of good movie to watch for a computer scientist?");
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine(result);
+// Invoke the prompt  
+var currentDay = await kernel.InvokeAsync("TimePlugin", "DayOfWeek");
+Console.WriteLine(currentDay);
