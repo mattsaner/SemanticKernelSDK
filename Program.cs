7d915d1a066b2457fde74cdcbd51f1e62ea6bec8
Matthieu <matthieu@sanerweb.com>
2024-11-30 09:06:38 +0100

    integration of the SemanticKernel SDK - Asking some good movies

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel; // For OpenAI Service interaction
+
+
+//Define OpenAI Service connection details
+var deploymentName = "MastercamCopilot";
+var endPoint = "https://az2005-training.openai.azure.com/";
+var apiKey = Environment.GetEnvironmentVariable("OPEN_AI_SERVICE_KEY");
+var modelId = "gpt-4o";
+const string switchInputMode = "Switch input mode";
+
+// Build Semantic Kernel
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(deploymentName, endPoint, apiKey, modelId);
+var kernel = builder.Build();       
+
+var result = await kernel.InvokePromptAsync("Give me a list of good movie to watch for a computer scientist?");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(result);
